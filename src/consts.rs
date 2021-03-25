71c9deecc3abe017a529c9a0a285d4cb31a107b5
daite <blueskykind02@yahoo.co.jp>
2021-03-25 20:38:29 +0900

    updated

--- src/consts.rs
+++ src/consts.rs
@@ -1,5 +1,5 @@
 pub const MY_USER_AGENT: &str = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1)";
 pub const TORRENTSIR_HOST_URL: &str = "https://torrentsir31.com";
-pub const TTOBOGO_HOST_URL: &str = "https://ttobogo.net";
+pub const TTOBOGO_HOST_URL: &str = "https://www2.ttobogo.net";
 pub const TORRENTMAX_HOST_URL: &str = "https://torrentmax15.com";
 pub const TORRENTQQ_HOST_URL: &str = "https://torrentqq75.com";
